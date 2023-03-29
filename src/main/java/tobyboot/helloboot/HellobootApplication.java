18a881e88a21065e6bf9a71e28b2a7da48124b23
jaehee <jaeheepark0145@gmail.com>
2023-03-29 15:11:08 +0900

    front controller 적용

--- src/main/java/tobyboot/helloboot/HellobootApplication.java
+++ src/main/java/tobyboot/helloboot/HellobootApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -20,27 +21,39 @@ import java.io.IOException;
 @SpringBootApplication
 public class HellobootApplication {
 
-	public static void main(String[] args) {
-		// 서블릿 컨테이너 띄우기
-		TomcatServletWebServerFactory serverF = new TomcatServletWebServerFactory();
-		// ServletWebServerFactory를 상속
-		WebServer webServer = serverF.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					String name = req.getParameter("name");
+    public static void main(String[] args) {
+        // 서블릿 컨테이너 띄우기
+        TomcatServletWebServerFactory serverF = new TomcatServletWebServerFactory();
+        // ServletWebServerFactory를 상속
+        WebServer webServer = serverF.getWebServer(servletContext -> {
+            servletContext.addServlet("front_controller", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-					// 웹 응답의 3요소
-					resp.setStatus(HttpStatus.OK.value());
-					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-					resp.getWriter().print("Hello " + name);
-				}
-			}).addMapping("/hello"); // 서블릿 지정
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(RequestMethod.GET.name())) {
 
-		});
-		webServer.start();
-	}
+                        String name = req.getParameter("name");
+                        // 웹 응답의 3요소
+                        resp.setStatus(HttpStatus.OK.value());
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().print("Hello " + name);
+                    } else {
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
+                }
+//			}).addMapping("/hello"); // 서블릿 지정
+            }).addMapping("/*"); // 프론트 컨트롤러로 모든 url 처리
 
+        });
+        webServer.start();
+
+        // 프론트 컨트롤러
+        // 기본적인 서블릿의 한계
+        // 여러개의 서블릿을 각각의 url에 매핑하는데 대부분의 서블릿에서 필요한 코드가 각 서블릿에 중복됨
+        // req, res를 처리하는 것이 부자연스럽다?
+        // 공통적인 부분을 프론트 컨트롤러에서 처리하자는 의미
+        // 인증, 보안, 다국어처리 등을 프런트 컨트롤러에서
+    }
 
 
 }
