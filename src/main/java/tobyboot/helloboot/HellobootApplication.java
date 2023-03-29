584576c73b038703b3ea00fa57d77c35487872d8
jaehee <jaeheepark0145@gmail.com>
2023-03-29 14:47:59 +0900

    servlet 등록, 요청 처리

--- src/main/java/tobyboot/helloboot/HellobootApplication.java
+++ src/main/java/tobyboot/helloboot/HellobootApplication.java
@@ -1,13 +1,46 @@
 package tobyboot.helloboot;
 
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @SpringBootApplication
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+		// 서블릿 컨테이너 띄우기
+		TomcatServletWebServerFactory serverF = new TomcatServletWebServerFactory();
+		// ServletWebServerFactory를 상속
+		WebServer webServer = serverF.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					String name = req.getParameter("name");
+
+					// 웹 응답의 3요소
+					resp.setStatus(HttpStatus.OK.value());
+					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+					resp.getWriter().print("Hello " + name);
+				}
+			}).addMapping("/hello"); // 서블릿 지정
+
+		});
+		webServer.start();
 	}
+
+
 
 }
